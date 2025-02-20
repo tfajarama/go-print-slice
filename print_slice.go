7ac935465925b36dfe890227375d6b72f93ee7a1
Taufan FP Ruslanali <tfpruslanali@gmail.com>
2025-02-20 11:59:43 +0700

    feat: print slice 2D

--- print_slice.go
+++ print_slice.go
@@ -8,3 +8,13 @@ func PrintSlice1D(slice []int) {
 	}
 	fmt.Println()
 }
+
+func PrintSlice2D(slice [][]int) {
+	for i := 0; i < len(slice); i++ {
+		for j := i + 1; j < len(slice); j++ {
+			fmt.Print(slice[i][j], " ")
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
