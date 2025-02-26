20e7f6f6c62bf10f67eb9a90f943effafb8d4a2b
Taufan Fajarama P.R. <taufan.fajarama@amartha.com>
2025-02-26 09:46:03 +0700

    refactor: print slice 2D print zeros with strip

--- print_slice.go
+++ print_slice.go
@@ -20,7 +20,7 @@ func PrintSlice2D(slice [][]int) {
 	for i := 0; i < len(slice); i++ {
 		for j := 0; j < len(slice[i]); j++ {
 			if slice[i][j] == 0 {
-				fmt.Print(" \t")
+				fmt.Print("-\t")
 			} else {
 				fmt.Print(slice[i][j], "\t")
 			}
